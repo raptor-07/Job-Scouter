ef6d027fb7700652a605bea3bc217f8124444c00
Hurmaan <contactme.hurmaan@gmail.com>
2024-03-31 11:27:11 +0530

    feat: create input component to load pdf/application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,105 @@
+import { Container, Paper, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { useRef } from "react";
+
 function App() {
-  return <>hello</>;
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
+
+  const handleDragLeave = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    if (e.dataTransfer.files) {
+      let files = Array.from(e.dataTransfer.files);
+      files = files.filter((file: any) => file.type === "application/pdf"); // Only keep PDF files
+      console.log(files); // Do something with the files
+    }
+  };
+
+  const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      let files = Array.from(e.target.files);
+      files = files.filter((file: any) => file.type === "application/pdf"); // Only keep PDF files
+      console.log(files); // Do something with the files
+    }
+  };
+
+  const handleClick = () => {
+    fileInputRef.current?.click();
+  };
+  return (
+    <>
+      <Container
+        disableGutters
+        sx={{
+          minWidth: "100%",
+          minHeight: "100vh",
+          margin: 0,
+          padding: 0,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            margin: 2,
+            padding: 2,
+            minWidth: "30vw",
+            minHeight: "30vh",
+            border: "8px dotted #a8a5a5",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          onDragOver={handleDragOver}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+        >
+          <Button
+            variant="contained"
+            startIcon={<AddIcon />}
+            onClick={handleClick}
+            sx={{ 
+              backgroundColor: "transparent", 
+              color: "white", 
+              fontWeight: "bold",
+              "&:hover": {
+                backgroundColor: "transparent",
+                boxShadow: "none",
+              },
+              border: "0px",
+              boxShadow: "0px 0px 6px 0px",
+              transition: "background-color 0.8s, box-shadow 0.8s, color 0.8s, transform 0.8s"
+            }}
+          >
+            <p style={{
+              margin: 0,
+              padding: 10,
+              textAlign: "center",
+              textWrap: "wrap",
+            }}>
+            Drag and drop your PDF files here
+            </p>
+          </Button>
+          <input
+            ref={fileInputRef}
+            type="file"
+            style={{ display: "none" }}
+            accept=".pdf"
+            onChange={handleFileSelect}
+          />
+        </Paper>
+      </Container>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <CssBaseline />
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
+);
