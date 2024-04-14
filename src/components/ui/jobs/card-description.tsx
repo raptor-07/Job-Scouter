2dbe69d97afa1b441d86ba051ccf908090450893
Hurmaan <contactme.hurmaan@gmail.com>
2024-04-14 23:30:56 +0530

    feat: create card description

---
+++ src/components/ui/jobs/card-description.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { cn } from "@/utils/cn";
+
+export const CardDescription = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <p
+      className={cn(
+        "text-zinc-400 tracking-wide leading-relaxed text-sm",
+        className
+      )}
+    >
+      {children}
+    </p>
+  );
+};
