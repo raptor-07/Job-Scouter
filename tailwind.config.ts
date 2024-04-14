696b63ac6970bd9dd9ea824f3aebb0a6b04d318c
Hurmaan <contactme.hurmaan@gmail.com>
2024-04-14 23:29:53 +0530

    util: dot background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,6 +57,11 @@ function addBackgroundHighlightDots({ matchUtilities, theme }: any) {
           `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
         )}")`,
       }),
+      "bg-dot": (value: any) => ({
+        backgroundImage: `url("${svgToTinyDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+        )}")`,
+      }),
     },
     { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
   );
