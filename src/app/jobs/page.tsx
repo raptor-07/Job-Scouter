b6883d54eb0a2fcc3d336ddde55ebba1b1724663
Hurmaan <contactme.hurmaan@gmail.com>
2024-04-14 23:44:57 +0530

    fix:

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -76,23 +76,4 @@ export const projects = [
   },
 ];
 
-const Icon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      // stroke-width="1.5"
-      // stroke="currentColor"
-      className="h-4 w-4 text-white stroke-2"
-    >
-      <path
-        // strokeLinecap="round"
-        // strokeLinejoin="round"
-        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-      />
-    </svg>
-  );
-};
-
 export default Page;
