a0e58fdeef5a26ce7cd3ebc6252ac63113a98907
Hurmaan <contactme.hurmaan@gmail.com>
2024-04-14 18:30:11 +0530

    feat: create signup page

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,40 +1,120 @@
 "use client";
 
 import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signupSchema } from "@/schemas/signupSchema";
 import { Label } from "@/components/ui/auth/label";
 import { Input } from "@/components/ui/auth/input";
 import { cn } from "@/utils/cn";
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-  IconBrandOnlyfans,
-} from "@tabler/icons-react";
+import { createUser } from "@/actions/createUser";
+import zod from "zod";
+
+type FormData = zod.infer<typeof signupSchema>;
 
 export default function Signup() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Form submitted");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormData>({
+    resolver: zodResolver(signupSchema),
+  });
+
+  const onSubmit = async (data: FormData) => {
+    console.log("onSubmit called", data);
+    try {
+      const result: any = await createUser(data);
+
+      if (result.error) {
+        console.error("Error creating user", result.error);
+        alert("Error creating user");
+        return;
+      }
+
+      alert("User created successfully");
+
+      reset();
+
+      return;
+    } catch (error) {
+      console.error("Error creating user", error);
+    }
   };
+
   return (
     <div className="min-h-[100vh] flex items-center justify-center ">
       <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 dark:shadow-[0px_0px_2px_2px_#bb9bf2] bg-white dark:bg-black min-h-[50vh]">
         <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200 text-center">
           Create your account on Job Scouter
         </h2>
-        <form className="my-8" onSubmit={handleSubmit}>
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-            <LabelInputContainer>
-              <Label htmlFor="username">User Name</Label>
-              <Input id="username" placeholder="Narayan Murthy" type="text" />
-            </LabelInputContainer>
-          </div>
+        <form
+          className="my-8"
+          onSubmit={handleSubmit(onSubmit, (errors) => console.log(errors))}
+        >
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="username">Username</Label>
+            <Input
+              id="username"
+              placeholder="Enter your username"
+              type="text"
+              {...register("username")}
+            />
+            {errors.username && (
+              <p
+                style={{
+                  color: "#fc382d",
+                  fontWeight: "bold",
+                  textShadow: "10px 10px 20px rgba(0,0,0,0.5)",
+                  fontSize: "0.8rem",
+                }}
+              >
+                {errors.username.message}
+              </p>
+            )}
+          </LabelInputContainer>
           <LabelInputContainer className="mb-4">
             <Label htmlFor="email">Email Address</Label>
-            <Input id="email" placeholder="NrnMurthy@infy.com" type="email" />
+            <Input
+              id="email"
+              placeholder="NrnMurthy@infy.com"
+              type="email"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p
+                style={{
+                  color: "#fc382d",
+                  fontWeight: "bold",
+                  textShadow: "10px 10px 20px rgba(0,0,0,0.5)",
+                  fontSize: "0.8rem",
+                }}
+              >
+                {errors.email.message}
+              </p>
+            )}
           </LabelInputContainer>
           <LabelInputContainer className="mb-4">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" placeholder="••••••••" type="password" />
+            <Input
+              id="password"
+              placeholder="••••••••"
+              type="password"
+              {...register("password")}
+            />
+            {errors.password && (
+              <p
+                style={{
+                  color: "#fc382d",
+                  fontWeight: "bold",
+                  textShadow: "10px 10px 20px rgba(0,0,0,0.5)",
+                  fontSize: "0.8rem",
+                }}
+              >
+                {errors.password.message}
+              </p>
+            )}
           </LabelInputContainer>
           <LabelInputContainer className="mb-8">
             <Label htmlFor="confirm-password">Confirm password</Label>
@@ -42,8 +122,23 @@ export default function Signup() {
               id="confirm-password"
               placeholder="••••••••"
               type="password"
+              {...register("confirmPassword")}
             />
+            {errors.confirmPassword && (
+              <p
+                style={{
+                  color: "#fc382d",
+                  fontWeight: "bold",
+                  textShadow: "10px 10px 20px rgba(0,0,0,0.5)",
+                  fontSize: "0.8rem",
+                }}
+              >
+                {errors.confirmPassword.message}
+              </p>
+            )}
           </LabelInputContainer>
+
+          <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
           <button
             className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium "
@@ -52,21 +147,6 @@ export default function Signup() {
             Sign up &rarr;
             <BottomGradient />
           </button>
-
-          <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-
-          <div className="flex flex-col space-y-4">
-            <button
-              className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-              type="submit"
-            >
-              <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-              <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                Google
-              </span>
-              <BottomGradient />
-            </button>
-          </div>
         </form>
       </div>
     </div>
