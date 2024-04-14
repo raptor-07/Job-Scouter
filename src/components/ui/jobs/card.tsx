895643650c96e3125378c152030cb74c14f7f137
Hurmaan <contactme.hurmaan@gmail.com>
2024-04-14 23:30:46 +0530

    feat: create card component

---
+++ src/components/ui/jobs/card.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { cn } from "@/utils/cn";
+
+export const Card = ({
+    className,
+    children,
+  }: {
+    className?: string;
+    children: React.ReactNode;
+  }) => {
+    return (
+      <div
+        className={cn(
+          "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-purple-200 relative z-20",
+          className
+        )}
+      >
+        <div className="relative z-50">
+          <div className="p-4">{children}</div>
+        </div>
+      </div>
+    );
+  };
+  
